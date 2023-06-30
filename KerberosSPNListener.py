ab5e10f5063c98034e1b3cf9c9852ab382f66d01
renekerr <rrenekerr@gmail.com>
2023-06-30 07:48:24 +0200

    ...

--- KerberosSPNListener.py
+++ KerberosSPNListener.py
@@ -1,11 +1,11 @@
 # Kerberos request (using a dictionary)
 
 # Standalone server
-items = {'server' : 'SRDWDES-01'}
+items = {'server' : 'server_name'}
 
 # Cluster
-#items = {'server01': 'SRDPISQLPRO-01', 'server02':'SRDPISQLPRO-02', 'listener': 'SRDPISQL-PRO'}
-user ='admindata'
+#items = {'server01': 'server_name_01', 'server02':'server_name_02', 'listener': 'listener_name'}
+user ='user_account'
 
 for n in items.values():
     print('--', n, '\n# Asignamos SPN de Usuario')
@@ -17,8 +17,8 @@ for n in items.values():
 
 
 # -- Habilitar KERBEROS para URL
-# setspn -s http/srpbirepsql-pre.mutua.es mutua\adminreport
-# setspn -s http/srpbirepsql-pre.mutua.es:1433 mutua\adminreport
+# setspn -s http/servername.domain.es domain\user
+# setspn -s http/servername.domain.es:1433 domain\user
 
 
 
